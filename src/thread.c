99b355c296d069abb05abf8e60a7982a53cc8ce6
Margoum Saad <margoumsaad3@gmail.com>
2021-04-07 22:51:49 +0200

    optimization

--- src/thread.c
+++ src/thread.c
@@ -14,12 +14,12 @@ struct thread {
     unsigned int flags;
     int valgrind_stackid;
     ucontext_t uctx;
-    STAILQ_ENTRY(thread) threads;
+    TAILQ_ENTRY(thread) threads;
 };
 
 // init FIFO for runnable threads
-typedef STAILQ_HEAD(runnable_fifo, thread) runnable_hd_t;
-runnable_hd_t runnable_hd = STAILQ_HEAD_INITIALIZER(runnable_hd);
+typedef TAILQ_HEAD(runnable_fifo, thread) runnable_hd_t;
+runnable_hd_t runnable_hd = TAILQ_HEAD_INITIALIZER(runnable_hd);
 
 static int main_initialized;
 
@@ -30,8 +30,8 @@ static int main_initialized;
  */
 void free_main_thread(void) {
     // if main wasn't freed due to a join by another thread
-    if (!STAILQ_EMPTY(&runnable_hd)) {
-        struct thread *th = STAILQ_FIRST(&runnable_hd);
+    if (!TAILQ_EMPTY(&runnable_hd)) {
+        struct thread *th = TAILQ_FIRST(&runnable_hd);
         VALGRIND_STACK_DEREGISTER(th->valgrind_stackid);
         free(th->uctx.uc_stack.ss_sp);
         free(th);
@@ -62,7 +62,7 @@ int init_main_thread(void) {
                                                     main_th->uctx.uc_stack.ss_sp + main_th->uctx.uc_stack.ss_size);
 
     // add main thread to runnable fifo
-    STAILQ_INSERT_HEAD(&runnable_hd, main_th, threads);
+    TAILQ_INSERT_HEAD(&runnable_hd, main_th, threads);
 
     return 0;
 }
@@ -77,10 +77,10 @@ int init_main_thread(void) {
  */
 void thread_exit(void *retval) {
     // get the thread at the head of runnable FIFO
-    struct thread *curr_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
     // remove it from runnable FIFO
-    STAILQ_REMOVE_HEAD(&runnable_hd, threads);
+    TAILQ_REMOVE(&runnable_hd,curr_th, threads);
 
     // set retval in the thread structure
     curr_th->retval = retval;
@@ -89,8 +89,8 @@ void thread_exit(void *retval) {
     curr_th->flags |= JOINABLE;
 
     // set context of the next thread in runnable FIFO
-    if (!STAILQ_EMPTY(&runnable_hd)) {
-        struct thread *next_th = STAILQ_FIRST(&runnable_hd);
+    if (!TAILQ_EMPTY(&runnable_hd)) {
+        struct thread *next_th = TAILQ_FIRST(&runnable_hd);
         setcontext(&next_th->uctx);
     }
 
@@ -99,7 +99,7 @@ void thread_exit(void *retval) {
 
 void thread_runner(void) {
     // get the thread at the head of runnable FIFO
-    struct thread *curr_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
     // call the entry function of the thread and pass the return value to thread_exit
     thread_exit(curr_th->func(curr_th->funcarg));
@@ -114,7 +114,7 @@ thread_t thread_self(void) {
     }
 
     // get the calling thread from runnable fifo
-    struct thread *curr_thread = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_thread = TAILQ_FIRST(&runnable_hd);
     return (thread_t)curr_thread;
 }
 
@@ -143,7 +143,7 @@ int thread_create(thread_t *newthread, void *(*func)(void *), void *funcarg) {
     makecontext(&thn->uctx, thread_runner, 0);
 
     // add new thread to runnable FIFO
-    STAILQ_INSERT_TAIL(&runnable_hd, thn, threads);
+    TAILQ_INSERT_TAIL(&runnable_hd, thn, threads);
 
     return 0;
 }
@@ -157,24 +157,49 @@ int thread_yield(void) {
     }
 
     // get the thread who yielded from head of runnable FIFO
-    struct thread *curr_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
     // remove it from runnable FIFO
-    STAILQ_REMOVE_HEAD(&runnable_hd, threads);
+    TAILQ_REMOVE(&runnable_hd,curr_th,threads);
 
     // insert it at tail
-    STAILQ_INSERT_TAIL(&runnable_hd, curr_th, threads);
+    TAILQ_INSERT_TAIL(&runnable_hd, curr_th, threads);
 
     // get the next thread in runnable FIFO
-    struct thread *next_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *next_th = TAILQ_FIRST(&runnable_hd);
 
     // swap to the context of next thread
     swapcontext(&curr_th->uctx, &next_th->uctx);
 
     return 0;
 }
+/* Cette fonction permet de passer la main au thread t*/
+int thread_yield_to(struct thread * t) {
+    // initialize the main thread if not done already
+    if (!main_initialized) {
+        init_main_thread();
+    }
 
+    // get the thread who yielded from head of runnable FIFO
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
+    // remove it from runnable FIFO
+    TAILQ_REMOVE(&runnable_hd,curr_th,threads);
+
+    // insert it at tail
+    TAILQ_INSERT_TAIL(&runnable_hd, curr_th, threads);
+    
+    // remove the thread t from the list
+    TAILQ_REMOVE(&runnable_hd,t,threads);
+
+    // Inserting the thread t in the head of the FIFO
+    TAILQ_INSERT_HEAD(&runnable_hd, t, threads);
+
+    // swap to the context of next thread
+    swapcontext(&curr_th->uctx, &t->uctx);
+
+    return 0;
+}
 /* attendre la fin d'exécution d'un thread.
  * la valeur renvoyée par le thread est placée dans *retval.
  * si retval est NULL, la valeur de retour est ignorée.
@@ -224,7 +249,7 @@ int thread_mutex_destroy(thread_mutex_t *mutex) {
 }
 
 int thread_mutex_lock(thread_mutex_t *mutex) {
-    struct thread *curr_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
     //  we don't block if mutex not initialized/destroyed or owned by calling thread
     if (mutex == NULL || mutex->is_destroyed != 0 || mutex->locker == (thread_t)curr_th) {
@@ -233,8 +258,8 @@ int thread_mutex_lock(thread_mutex_t *mutex) {
 
     // otherwise we block until mutex is available
     while (mutex->locker != NULL) {
-        // eventually we could yield to locker thread directly ?
-        thread_yield();
+        // eventually we could yield to locker thread directly 
+        thread_yield_to(mutex->locker);
     }
 
     // lock the mutex
@@ -244,7 +269,7 @@ int thread_mutex_lock(thread_mutex_t *mutex) {
 }
 
 int thread_mutex_unlock(thread_mutex_t *mutex) {
-    struct thread *curr_th = STAILQ_FIRST(&runnable_hd);
+    struct thread *curr_th = TAILQ_FIRST(&runnable_hd);
 
     // unlocking a mutex not owned by calling thread is an error
     if (mutex == NULL || mutex->is_destroyed != 0 || mutex->locker != (thread_t)curr_th) {
